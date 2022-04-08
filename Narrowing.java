2dba01421dfcce481d894cbf9836a4583f33d724
AarthiMahes <68096569+AarthiMahes@users.noreply.github.com>
2022-04-07 23:03:15 -0500

    Add files via upload

---
+++ Narrowing.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    double myDouble = 9.78d;
+    int myInt = myDouble;   //Replace
+   
+   
+  
+
+    System.out.println(myDouble);   
+    System.out.println(myInt);  
+    
+     // int myInt = (int) myDouble; // Manual casting: double to int
+  }
+}
